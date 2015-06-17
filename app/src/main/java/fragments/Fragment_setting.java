931303b99bd613bd4dd73cfa5308632cbf6ace66
winhtaikaung <winhtaikaung28@hotmail.com>
2015-06-17 15:24:36 +0630

    Detail activity linked and new Fragment Setting added

---
+++ app/src/main/java/fragments/Fragment_setting.java
@@ -0,0 +1,31 @@
+package fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicmyanmar.jr.ramdhantimetable.R;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by jr on 6/17/15.
+ */
+public class Fragment_setting extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
+
+        return view;
+    }
+}
